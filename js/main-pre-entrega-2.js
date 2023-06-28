1eb14256f07f51655954b2984475b8143e8a49ee
octavio murua <octavioAISM@gmail.com>
2023-06-28 11:23:11 -0300

    Actualizacion de CSS, Sweet Alert, inicio de desarrollo del carrito

--- js/main-pre-entrega-2.js
+++ js/main-pre-entrega-2.js
@@ -1,53 +1,66 @@
-const carrito = JSON.parse(localStorage.getItem("carrito")) || []; //en caso de haber un carrito guardado en el LS lo toma y continua desde ahi
-const ventas = JSON.parse(localStorage.getItem("ventas")) || []; //toma el registro de todas las ventas realizadas y guardadas en el LS.
-const ingresosTotales = JSON.parse(localStorage.getItem("ingresosTotales")) || 0; //toma el valor de todas las ventas realizadas y guardas en LS
+const modal = document.getElementById("modalLayer");
+
+const carritoInLocalStorage = JSON.parse(localStorage.getItem("carrito")); //en caso de haber un carrito guardado en el LS lo toma y continua desde ahi
+let carrito = carritoInLocalStorage || []; // crear un carrito nuevo, dependiendo de si hay un carrito en el local storage, y si no crea uno nuevo sin productos agregados
+
+const ventasInLocalStorage = JSON.parse(localStorage.getItem("ventas")); //toma el registro de todas las ventas realizadas y guardadas en el LS.
+let ventas = ventasInLocalStorage || []; //crea un array donde se basa en el array de ventas guardado en el LS y sino crea uno nuevo sin ventas registradas
+
+const ingresosTotalesInLocalStorage = JSON.parse(localStorage.getItem("ingresosTotales")); //toma el valor de todas las ventas realizadas y guardas en LS
+let ingresosTotales = ingresosTotalesInLocalStorage || 0; //asigna un valor de ingresos totales si es que se efectuaron ventas, o inicia en 0
 
 const containerCards = document.getElementById("containerCards");
 
 const btnComprar = document.getElementById("btnComprar");
 const btnVerCompras = document.getElementById("btnVerCompras");
+const btnVerCarrito = document.getElementById("btnVerCarrito");
 
 
-function confirmarCompra(){
+function comprar(){ //crea un objeto compra, calcula el monto final de la compra (con o sin recargo), guarda la compra en el array de ventas, lo almacena en el LS y finalmente vacia el carrito para una nueva compra.
+    let tarjeta = false;
+    let confirmarCompra = false;
+
     Swal.fire({ //alert que pregunta si quieres concretar la compra
         title: 'Seguro quieres terminar la compra?',
         text: "Si confirmas no podras volver atras!",
         icon: 'warning',
+        background: 'white',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, lo quiero!'
-        })
-        .then((result) =>  { return result.isConfirmed })
-        .catch(err => { console.log(err); return false; });
-};
+        confirmButtonColor: '#888',
+        cancelButtonColor: '#CCCCCC',
+        confirmButtonText: 'Si, lo quiero!' 
+        }).then((result) => { //evalua el resultado del alert y re asigna el valor booleando del parametro "confirmarCompra"
+        if (result.isConfirmed) {
+            confirmarCompra = true;
+        }
+        if(confirmarCompra){
+            Swal.fire({ //una vez confirmada la genera un alert que pregunta si quieres pagar con tarjeta
+                title: 'Quieres pagar con tarjeta?',
+                text: "Si lo haces tendras un 10% de recargo!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#888',
+                cancelButtonColor: '#CCCCCC',
+                confirmButtonText: 'Si!',
+                cancelButtonText: 'No!'
+                }).then((result) => {
+                if (result.isConfirmed) { //reasigna en caso de ser necesario el valor booleando de "tarjeta"
+                    tarjeta = true;
+                }
 
-function recargoTarjeta(){
-    Swal.fire({ //una vez confirmada la genera un alert que pregunta si quieres pagar con tarjeta
-        title: 'Quieres pagar con tarjeta?',
-        text: "Si lo haces tendras un 10% de recargo!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si!'
-        })
-        .then((result) => { return result.isConfirmed })
-        .catch(err => { console.log(err); return false; });
-}
 
-function comprar(){ //crea un objeto compra, calcula el monto final de la compra (con o sin recargo), guarda la compra en el array de ventas, lo almacena en el LS y finalmente vacia el carrito para una nueva compra.
-
-    if(confirmarCompra()){
-        const compra = new Compra(carrito); //a partir de aca, concreta la compra copiando el carrito al objeto compra
-
-        compra.calcularTotal(recargoTarjeta()); //calcula el monto final
-        registrarCompra(compra); //registra la compra en un array de compras realizadas
-        localStorage.setItem("ventas", JSON.stringify(ventas)) //convierte el array anterior en un json y lo guarda en el local storage
+                const compra = new Compra(carrito); //a partir de aca, concreta la compra copiando el carrito al objeto compra
     
-        carrito.splice(0,carrito.length) //vacia el carrito y lo deja en 0 para realizar una nueva compra
-        localStorage.setItem("carrito", JSON.stringify(carrito)); //carga el nuevo carrito al local, actualizandolo
-    }
+                compra.calcularTotal(tarjeta); //calcula el monto final
+                registrarCompra(compra); //registra la compra en un array de compras realizadas
+                localStorage.setItem("ventas", JSON.stringify(ventas)) //convierte el array anterior en un json y lo guarda en el local storage
+            
+                carrito.splice(0,carrito.length) //vacia el carrito y lo deja en 0 para realizar una nueva compra
+                localStorage.setItem("carrito", JSON.stringify(carrito)); //carga el nuevo carrito al local, actualizandolo
+            })
+            }
+    
+    })
 }
 
 function registrarCompra(compra){ //guarda un objeto compra en el vector de "ventas".
@@ -64,6 +77,7 @@ function registrarCompra(compra){ //guarda un objeto compra en el vector de "ven
 }
 
 function verCompras(){ //metodo para ver la cantidad de ventas y el monto total de todas ellas
+    modal.style.display = "visible";
     const cantidadVentas = JSON.parse(localStorage.getItem("ventas"));
     
     ventasTotales = cantidadVentas.length;
@@ -72,6 +86,19 @@ function verCompras(){ //metodo para ver la cantidad de ventas y el monto total 
     alert(`Ventas totales: ${ventasTotales} Ingresos totales del dia: $${ingresosTotales}`);
 }
 
+function verCarrito(){
+    modal.style.display = 'block';
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
+}
+
+
+
+btnVerCarrito.addEventListener("click", verCarrito);
 btnComprar.addEventListener("click", comprar);
 btnVerCompras.addEventListener("click", verCompras);
 
